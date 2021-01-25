ffbd90e601add43d23301833b9d26697cbc62507
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-25 14:16:20 -0600

    lint fixes

--- js/test/Evaluator.test.js
+++ js/test/Evaluator.test.js
@@ -9,26 +9,26 @@ describe('Evaluator.evalTruth', () => {
   ${'trivial false'} | ${'false'} | ${{}} | ${false}
   ${'trivial true'} | ${'1'} | ${{}} | ${true}
   ${'trivial true'} | ${'true'} | ${{}} | ${true}
-  ${'extraneous parameters'} | ${'true'} | ${{ blah: 0 }} | ${true}
-  ${'simple parameter sub'} | ${'FOO'} | ${{ FOO: 1 }} | ${true}
-  ${'complex expression'} | ${'BAR || (FOO && 1)'} | ${{ BAR: 'false', FOO: 1 }} | ${true}
-  ${'simple math'}| ${'2 + BAR - FOO == 3'} | ${{ BAR: 4, FOO: 3 }} | ${true}
-  ${'complex math'}| ${'(BAR % 2 == 0) && (FOO * 3 != 6)'} | ${{ BAR: 4, FOO: 3 }} | ${true}
-  ${'not expression'} | ${'!BAR'} | ${{ BAR: 1 }} | ${false}
-  ${'complex not expression'} | ${'FOO && !BAR'} | ${{ FOO: 1, BAR: false }} | ${true}
+  ${'extraneous parameters'} | ${'true'} | ${{ blah : 0 }} | ${true}
+  ${'simple parameter sub'} | ${'FOO'} | ${{ FOO : 1 }} | ${true}
+  ${'complex expression'} | ${'BAR || (FOO && 1)'} | ${{ BAR : 'false', FOO : 1 }} | ${true}
+  ${'simple math'}| ${'2 + BAR - FOO == 3'} | ${{ BAR : 4, FOO : 3 }} | ${true}
+  ${'complex math'}| ${'(BAR % 2 == 0) && (FOO * 3 != 6)'} | ${{ BAR : 4, FOO : 3 }} | ${true}
+  ${'not expression'} | ${'!BAR'} | ${{ BAR : 1 }} | ${false}
+  ${'complex not expression'} | ${'FOO && !BAR'} | ${{ FOO : 1, BAR : false }} | ${true}
   `("$desc; '$expression' with conditions '$parameters' -> $result'", ({ desc, expression, parameters, result }) => {
-    const evaluator = new Evaluator({parameters: parameters})
-    expect(evaluator.evalTruth(expression)).toBe(result)
-  })
+  const evaluator = new Evaluator({ parameters : parameters })
+  expect(evaluator.evalTruth(expression)).toBe(result)
+})
 
   test('falls back to process.env for substitution', () => {
     process.env.FOO = 'true'
-    const evaluator = new Evaluator({parameters: { BAR: 'false' }})
+    const evaluator = new Evaluator({ parameters : { BAR : 'false' } })
     expect(evaluator.evalTruth('BAR || FOO')).toBe(true)
   })
 
   test('utilizes zeroRes', () => {
-    const evaluator = new Evaluator({zeroRes: [/^.+_ZERO_PARAM/]})
+    const evaluator = new Evaluator({ zeroRes : [/^.+_ZERO_PARAM/] })
     expect(evaluator.evalTruth('SOME_ZERO_PARAM')).toBe(false)
   })
 
@@ -37,20 +37,20 @@ describe('Evaluator.evalTruth', () => {
   ${'TRUE'}
   ${'FALSE'}
   ${'UNKNOWN'}
-  `("rejects unknown parameter '$param'", ({param}) => {
-    const evaluator = new Evaluator()
-    const expression = `true || ${param}`
-    expect(() => evaluator.evalTruth(expression)).toThrow(new RegExp(`^Condition parameter '${param}' is not defined.`))
-  })
+  `("rejects unknown parameter '$param'", ({ param }) => {
+  const evaluator = new Evaluator()
+  const expression = `true || ${param}`
+  expect(() => evaluator.evalTruth(expression)).toThrow(new RegExp(`^Condition parameter '${param}' is not defined.`))
+})
 
   test.each`
   expression
   ${'someFunc()'}
   ${'~1'}
   `("rejects unsafe expression '$expression'", ({ expression }) => {
-    const evaluator = new Evaluator()
-    expect(() => evaluator.evalTruth(expression)).toThrow(/Invalid expression/)
-  })
+  const evaluator = new Evaluator()
+  expect(() => evaluator.evalTruth(expression)).toThrow(/Invalid expression/)
+})
 
   test.each`
   input
@@ -59,7 +59,7 @@ describe('Evaluator.evalTruth', () => {
   ${1}
   ${true}
   `("rejects non-string input '$input'", ({ input }) => {
-    const evaluator = new Evaluator()
-    expect(() => evaluator.evalTruth(input)).toThrow(/^Expression must be a string./)
-  })
+  const evaluator = new Evaluator()
+  expect(() => evaluator.evalTruth(input)).toThrow(/^Expression must be a string./)
+})
 })
