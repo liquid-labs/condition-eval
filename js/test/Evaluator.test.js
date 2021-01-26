eeadba1e1ca77c888c40d3668d1a7e42d6a8269e
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-26 01:26:30 -0600

    hotfix: lint formating

--- js/test/Evaluator.test.js
+++ js/test/Evaluator.test.js
@@ -12,8 +12,8 @@ describe('Evaluator.evalTruth', () => {
   ${'extraneous parameters'} | ${'true'} | ${{ blah : 0 }} | ${true}
   ${'simple parameter sub'} | ${'FOO'} | ${{ FOO : 1 }} | ${true}
   // This was failing at one point; not sure why
-  ${'simple parameter sub - unkown bug check'} | ${'BUSINESS'} | ${{ BUSINESS: 1 }} | ${true}
-  ${'simple parameter sub - single char'} | ${'B'} | ${{ B: 1 }} | ${true}
+  ${'simple parameter sub - unkown bug check'} | ${'BUSINESS'} | ${{ BUSINESS : 1 }} | ${true}
+  ${'simple parameter sub - single char'} | ${'B'} | ${{ B : 1 }} | ${true}
   ${'complex expression'} | ${'BAR || (FOO && 1)'} | ${{ BAR : 'false', FOO : 1 }} | ${true}
   ${'simple math'}| ${'2 + BAR - FOO == 3'} | ${{ BAR : 4, FOO : 3 }} | ${true}
   ${'complex math'}| ${'(BAR % 2 == 0) && (FOO * 3 != 6)'} | ${{ BAR : 4, FOO : 3 }} | ${true}
