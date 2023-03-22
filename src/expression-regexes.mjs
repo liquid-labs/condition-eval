fd596b5b980f46343412209a14564efae1ece10b
Zane Rockenbaugh <zane@liquid-labs.com>
2023-03-22 12:45:15 -0500

    lint fixes

--- src/expression-regexes.mjs
+++ src/expression-regexes.mjs
@@ -3,13 +3,13 @@ const dualOp = '(?:&&|[|]{2}|==|!=|[*]{2}|[+%*/^&|<>-]|<=|>=|<<|>>)'
 const anyOp = `(?:${singleOp}|${dualOp})`
 const literal = '(?:-?[0-9]+|false|true)'
 const param = '(?:[A-Z_][A-Z0-9_]*)'
-const anyValue = `(?:${literal}|${param})`
+// const anyValue = `(?:${literal}|${param})`
 const opener = `(?:[ (]|${singleOp})`
 const closer = '[ )]'
 
 const paramRe = new RegExp(`(?:^|${opener}|${anyOp})(${param})`, 'g')
 
 const dualOpChain = `(?:${opener}*${dualOp}${opener}*${literal}${closer}*)`
-const safeEvalRe = new RegExp(`^\s*(?:${opener}*${literal}${dualOpChain}*${closer}*)?$`)
+const safeEvalRe = new RegExp(`^\\s*(?:${opener}*${literal}${dualOpChain}*${closer}*)?$`)
 
 export { paramRe, safeEvalRe }
